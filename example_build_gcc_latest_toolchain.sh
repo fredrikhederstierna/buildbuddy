b045d3dcfb87cb0ac2c5911db72580656ee5fbe5
fredrikhederstierna <fredrik@hederstierna.com>
2019-02-24 18:52:29 +0100

    added latest build

---
+++ example_build_gcc_latest_toolchain.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Example non-interactive toolchain build argument list
+# Fredrik Hederstierna 2019
+
+INTERACTIVE="No"
+TARGET="arm-none-eabi"
+WITH_CPU="cortex-m4"
+ENABLE_MULTILIB="Y"
+PARALLEL_MAKE="Y"
+LANGUAGES="c,c++"
+BINUTILS_VERSION="2.32"
+GCC_VERSION="8.3.0"
+NEWLIB_VERSION="3.1.0"
+GDB_VERSION="8.2.1"
+DEST_PATH="/opt/gcc"
+DEST_PATH_SUFFIX=""
+HARDFLOAT="Y"
+STATIC="N"
+BUILD_GDB="Y"
+BUILD_RPM="N"
+SUDO_INSTALL="Y"
+APPLY_PATCH="Y"
+DOWNLOAD_GNU_SERVER="http://ftp.gnu.org/gnu"
+DOWNLOAD_NEWLIB_SERVER="http://sourceware.org/pub/newlib"
+WGET_HTTP_PROXY_EXTRA=""
+#WGET_HTTP_PROXY_EXTRA="-e use_proxy=yes -e http_proxy=xxx.xxx.xxx.xxx:yyyy"
+WGET_FTP_PROXY_EXTRA=""
+#WGET_FTP_PROXY_EXTRA="-e use_proxy=yes -e ftp_proxy=xxx.xxx.xxx.xxx:yyyy"
+
+source ./gnu_toolchain_build_buddy.sh
